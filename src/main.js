639859fb63f82f7c154111bb0cb3ba8c13a54ea6
TYHN <1395421087@qq.com>
2022-07-16 00:02:35 +0800

    work7-15

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import "bootstrap/dist/css/bootstrap.css"
+
+
+//注册全局的过滤器
+Vue.filter('toLow',(val) => {
+  return val.toLowerCase()
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
